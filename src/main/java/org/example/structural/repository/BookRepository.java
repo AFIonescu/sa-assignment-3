cdbe923b6b61ccc325034961f6dba50247fbbcac
Alexandru-Florin Ionescu <afionescu@alex.ionescu97@yahoo.com>
2026-01-16 00:49:20 +0200

    Fix

--- src/main/java/org/example/structural/repository/BookRepository.java
+++ src/main/java/org/example/structural/repository/BookRepository.java
@@ -5,5 +5,4 @@ import org.example.structural.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    // Additional query methods can be defined here if needed
 }
